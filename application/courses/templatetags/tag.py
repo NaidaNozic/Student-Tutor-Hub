2d79f4c12080fe8878bfdce5eeb3ccd82397c5de
NaidaNozic <92310390+NaidaNozic@users.noreply.github.com>
2023-03-10 22:32:03 +0100

    View replies with bootstrap modal

--- application/courses/templatetags/tag.py
+++ application/courses/templatetags/tag.py
@@ -1,11 +1,18 @@
 from django import template
-from ..models import Material
+from ..models import Material,Answer
 
 register = template.Library()
 
 @register.simple_tag
 def getMaterials(notice):
     
-    materials=Material.objects.filter(notice=notice)
+    materials = Material.objects.filter(notice=notice)
     
     return materials
+
+@register.simple_tag
+def getAnswers(question):
+
+    answers = Answer.objects.filter(question=question)
+    
+    return answers
